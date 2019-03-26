7919035e088d8bd2ab7b7e0f0caed799b904c13b
Cjay1019 <cjay741@gmail.com>
2019-03-26 19:29:12 -0400

    mdb init

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-light blue lighten-4">
+      <span className="navbar-brand h1">Heading</span>
+    </nav>
+  );
+}
+
+export default Navbar;
